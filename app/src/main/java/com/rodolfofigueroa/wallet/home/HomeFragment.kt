97cdbc094c50d8724c279ebb5eb630a732f55686
Rodolfo Figueroa <ficr23@hotmail.com>
2020-03-19 15:21:54 -0600

    Agregue en la vista el recyclerview, el cardView del recycler, funcionalidad al circularprogress y añadi la libreria de picasso

--- app/src/main/java/com/rodolfofigueroa/wallet/home/HomeFragment.kt
+++ app/src/main/java/com/rodolfofigueroa/wallet/home/HomeFragment.kt
@@ -6,12 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 
 import com.rodolfofigueroa.wallet.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_home.*
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -21,6 +22,17 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        circularProgress.setProgressWithAnimation(50f, 1000, AccelerateDecelerateInterpolator(), 500)
+        Picasso
+            .get()
+            .load("https://pbs.twimg.com/profile_images/625753908082642944/FlXPNd-9_400x400.jpg")
+            .into(circleImageView)
+    }
+
+
 
 
 }
